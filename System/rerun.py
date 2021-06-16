27f949c537370ae596033a11c5addccc97e97da8
voidedstarlight <haii@voided.systems>
2021-06-15 20:22:08 -0700

    implemented configurations (System/config.py and System/config/*)

--- System/rerun.py
+++ System/rerun.py
@@ -1,7 +1,8 @@
 """
 System/rerun.py
 Rerun main.py script for imports
+
 """
-import os
-print("Successfully installed required packages.")
-os.system("python3 System/main.py")
+def rerun():
+	print("Successfully installed required packages.")
+	__import__("os").system("python3 System/main.py")
